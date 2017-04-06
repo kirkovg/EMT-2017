69536c4749df9e72e886eaf12d9c54249bc955cf
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-04-07 00:36:42 +0200

    Tested StockServiceHelper

--- src/test/java/mk/ukim/finki/emt/service/StockHelperTest.java
+++ src/test/java/mk/ukim/finki/emt/service/StockHelperTest.java
@@ -1,6 +1,8 @@
 package mk.ukim.finki.emt.service;
 
 import mk.ukim.finki.emt.model.exceptions.NotEnoughStockException;
+import mk.ukim.finki.emt.model.jpa.Book;
+import mk.ukim.finki.emt.persistence.BookRepository;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,6 +23,12 @@ public class StockHelperTest {
     @Autowired
     StockServiceHelper stockServiceHelper;
 
+    @Autowired
+    BookRepository bookRepository;
+
+    private static final String AUTHOR_NAME = "Gjorgji Kirkov";
+
+
     @After
     public void clearTestEntities() {
 
@@ -28,12 +36,58 @@ public class StockHelperTest {
 
     @Test
     public void addBooksInStock() {
-        //Book book =
+        Book book = new Book();
+        book.quantityInStock = 3;
+        bookRepository.save(book);
+        stockServiceHelper.addBooksInStock(book.id, 3);
+        Book book2 = bookRepository.findOne(book.id);
+
+        Assert.assertNotNull(book2.id);
+        Assert.assertEquals("Not the same book",
+                book.id,
+                book2.id
+        );
+
+        Assert.assertEquals("Not the same quantity!",
+                6L,
+                (long) book2.quantityInStock
+        );
     }
 
     @Test
-    public void donateBooks() throws NotEnoughStockException {
+    public void donateBooksWithEnoughStock() throws NotEnoughStockException {
+        Book book = new Book();
+        book.quantityInStock = 3;
+        bookRepository.save(book);
 
+        stockServiceHelper.donateBooks(book.id, 3);
+        Book book2 = bookRepository.findOne(book.id);
+
+        Assert.assertNotNull(book2.id);
+        Assert.assertEquals("Not the same book",
+                book.id,
+                book2.id
+        );
+
+        Assert.assertEquals("Not the same quantity!",
+                0L,
+                (long) book2.quantityInStock
+        );
+    }
+
+    @Test
+    public void donateBooksWithoutEnoughStock() throws NotEnoughStockException {
+        Book book = new Book();
+        book.quantityInStock = 3;
+        bookRepository.save(book);
+
+        NotEnoughStockException expectedException = null;
+        try {
+            stockServiceHelper.donateBooks(book.id, 5);
+        } catch (NotEnoughStockException ex) {
+            expectedException = ex;
+        }
+        Assert.assertNotNull("NotEnoughStockException not thrown", expectedException);
     }
 
 }
